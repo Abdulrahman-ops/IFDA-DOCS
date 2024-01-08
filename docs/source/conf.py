9f401259c3aa0c4793eb9d2924174057707c6ab3
Abdulrahman Ali <aabbcc2000107@gmail.com>
2024-01-08 11:02:42 +0300

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -25,7 +25,7 @@ language = 'ar'
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
+#html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 
 
