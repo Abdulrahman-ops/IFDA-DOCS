457ad9231ee7e9c363908705c0ea9862512a35df
abdulrahman <aabbcc2000107@gmail.com>
2024-01-11 09:30:46 +0300

    adding custom css for the theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -32,6 +32,8 @@ html_theme = 'sphinx_rtd_theme'
 #html_theme_path = [sphinx_minoo_theme.get_html_theme_path()]
 
 html_static_path = ['_static']
+html_css_files = ['custom_theme/custom.css']
+
 
 
 
